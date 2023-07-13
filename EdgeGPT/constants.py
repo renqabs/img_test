31ad1feabb3e536a6f5ec2b01c4804e8f85907e1
renqabs <130155002+renqabs@users.noreply.github.com>
2023-07-13 22:22:54 +0800

    Update constants.py

--- EdgeGPT/constants.py
+++ EdgeGPT/constants.py
@@ -5,17 +5,17 @@ DELIMITER = "\x1e"
 
 
 # Generate random IP between range 13.104.0.0/14
-FORWARDED_IP = f"2.17.{random.randint(102, 106)}.{random.randint(1, 255)}"
+FORWARDED_IP = f"13.{random.randint(104, 107)}.{random.randint(0, 255)}.{random.randint(0, 255)}"
 
 HEADERS = {
     "accept": "application/json",
     "accept-language": "en-US,en;q=0.9",
     "content-type": "application/json",
-    "sec-ch-ua": '"Not_A Brand";v="99", Microsoft Edge";v="115", "Chromium";v="115"',
+    "sec-ch-ua": '"Not_A Brand";v="99", Microsoft Edge";v="110", "Chromium";v="110"',
     "sec-ch-ua-arch": '"x86"',
     "sec-ch-ua-bitness": '"64"',
-    "sec-ch-ua-full-version": '"115.0.1901.170"',
-    "sec-ch-ua-full-version-list": '"Chromium";v="115.0.5790.75", "Not A(Brand";v="99.0.0.0", "Microsoft Edge";v="115.0.5790.75"',
+    "sec-ch-ua-full-version": '"109.0.1518.78"',
+    "sec-ch-ua-full-version-list": '"Chromium";v="110.0.5481.192", "Not A(Brand";v="24.0.0.0", "Microsoft Edge";v="110.0.1587.69"',
     "sec-ch-ua-mobile": "?0",
     "sec-ch-ua-model": "",
     "sec-ch-ua-platform": '"Windows"',
@@ -35,20 +35,18 @@ HEADERS_INIT_CONVER = {
     "accept": "application/json",
     "accept-language": "en-US,en;q=0.9",
     "cache-control": "max-age=0",
-    "sec-ch-ua": '"Chromium";v="115", "Not A(Brand";v="99", "Microsoft Edge";v="115"',
+    "sec-ch-ua": '"Chromium";v="110", "Not A(Brand";v="24", "Microsoft Edge";v="110"',
     "sec-ch-ua-arch": '"x86"',
     "sec-ch-ua-bitness": '"64"',
-    "sec-ch-ua-full-version": '"115.0.1901.170"',
-    "sec-ch-ua-full-version-list": '"Chromium";v="115.0.5790.75", "Not A(Brand";v="99.0.0.0", "Microsoft Edge";v="115.0.1901.170"',
+    "sec-ch-ua-full-version": '"110.0.1587.69"',
+    "sec-ch-ua-full-version-list": '"Chromium";v="110.0.5481.192", "Not A(Brand";v="24.0.0.0", "Microsoft Edge";v="110.0.1587.69"',
     "sec-ch-ua-mobile": "?0",
     "sec-ch-ua-model": '""',
     "sec-ch-ua-platform": '"Windows"',
     "sec-ch-ua-platform-version": '"15.0.0"',
     "upgrade-insecure-requests": "1",
-    "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.0.0",
+    "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.46",
     "x-edge-shopping-flag": "1",
-    "Referrer-Policy": "origin-when-cross-origin",
-    "Referer": "https://www.bing.com/search?q=Bing+AI&showconv=1&FORM=hpcodx",
     "x-forwarded-for": FORWARDED_IP,
 }
 
