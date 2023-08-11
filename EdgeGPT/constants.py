f83c8ea4d8e4f82ccd344d79755a35c936dd934a
renqabs <130155002+renqabs@users.noreply.github.com>
2023-08-12 01:35:06 +0800

    Update constants.py

--- EdgeGPT/constants.py
+++ EdgeGPT/constants.py
@@ -13,6 +13,7 @@ HEADERS = {
     "pragma": "no-cache",
     "x-forwarded-for": FORWARDED_IP,
     "origin": "https://www.bing.com",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.0.0",
 }
 
 HEADERS_INIT_CONVER = {
