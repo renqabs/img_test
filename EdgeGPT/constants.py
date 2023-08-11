29901fcb0982fe70adeaef80fe85ba45fb675b2f
renqabs <130155002+renqabs@users.noreply.github.com>
2023-08-12 00:43:06 +0800

    Update constants.py

--- EdgeGPT/constants.py
+++ EdgeGPT/constants.py
@@ -18,23 +18,22 @@ HEADERS = {
 }
 
 HEADERS_INIT_CONVER = {
-    "accept": "*/*",
-    "accept-encoding": "gzip, deflate, br",
-    "accept-language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
-    "sec-ch-ua": '"Chromium";v="116", "Not)A;Brand";v="24", "Microsoft Edge";v="116"',
+    "accept": "application/json",
+    "accept-language": "en-US;q=0.9",
+    "sec-ch-ua": '"Not)A;Brand";v="24", "Microsoft Edge";v="116", "Chromium";v="116",',
     "sec-ch-ua-arch": '"x86"',
     "sec-ch-ua-bitness": '"64"',
     "sec-ch-ua-full-version": '"116.0.1938.43"',
-    "sec-ch-ua-full-version-list": '"Chromium";v="116.0.5845.62", "Not)A;Brand";v="24.0.0.0", "Microsoft Edge";v="116.0.1938.43"',
+    "sec-ch-ua-full-version-list": '"Not)A;Brand";v="24.0.0.0", "Microsoft Edge";v="116.0.1938.43", "Chromium";v="116.0.5845.62"',
     "sec-ch-ua-mobile": "?0",
     "sec-ch-ua-model": '""',
     "sec-ch-ua-platform": '"Windows"',
     "sec-ch-ua-platform-version": '"15.0.0"',
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.0.0",
     "x-forwarded-for": FORWARDED_IP,
-    "referrer-policy": "origin-when-cross-origin",
-    'referer': 'https://www.bing.com/search?q=Bing+AI&showconv=1&FORM=hpcodx',
 }
+
+
 
 HEADER_IMG_UPLOAD = {
     'referer': 'https://www.bing.com/search?q=Bing+AI&showconv=1&FORM=hpcodx',
