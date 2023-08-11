929da744ca8ddebcb75596f9abdad8231887d36a
renqabs <130155002+renqabs@users.noreply.github.com>
2023-08-12 01:29:21 +0800

    Add files via upload

---
+++ SydneyGPT/conversation_style.py
@@ -0,0 +1,68 @@
+from enum import Enum
+
+try:
+    from typing import Literal, Union
+except ImportError:
+    from typing_extensions import Literal
+from typing import Optional
+
+
+class ConversationStyle(Enum):
+    creative = [
+        "nlu_direct_response_filter",
+        "deepleo",
+        "disable_emoji_spoken_text",
+        "responsible_ai_policy_235",
+        "enablemm",
+        "dv3sugg",
+        "autosave",
+        "iyxapbing",
+        "iycapbing",
+        "h3imaginative",
+        "clgalileo",
+        "gencontentv3",
+        "fluxsrtrunc",
+        "fluxtrunc",
+        "fluxv1",
+        "rai278",
+        "replaceurl",
+        "iyoloexp",
+        "udt4upm5gnd",
+        "eredirecturl",
+        "nojbfedge",
+    ]
+    balanced = [
+        "nlu_direct_response_filter",
+        "deepleo",
+        "disable_emoji_spoken_text",
+        "responsible_ai_policy_235",
+        "enablemm",
+        "galileo",
+        "dv3sugg",
+        "responseos",
+        "e2ecachewrite",
+        "cachewriteext",
+        "nodlcpcwrite",
+        "travelansgnd",
+    ]
+    precise = [
+        "nlu_direct_response_filter",
+        "deepleo",
+        "disable_emoji_spoken_text",
+        "responsible_ai_policy_235",
+        "enablemm",
+        "galileo",
+        "dv3sugg",
+        "responseos",
+        "e2ecachewrite",
+        "cachewriteext",
+        "nodlcpcwrite",
+        "travelansgnd",
+        "h3precise",
+        "clgalileo",
+    ]
+
+
+CONVERSATION_STYLE_TYPE = Optional[
+    Union[ConversationStyle, Literal["creative", "balanced", "precise"]]
+]
