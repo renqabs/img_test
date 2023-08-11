a114574a7216d640ce2d6a45844f15344bf8b234
renqabs <130155002+renqabs@users.noreply.github.com>
2023-08-11 22:50:52 +0800

    Update constants.py

--- EdgeGPT/constants.py
+++ EdgeGPT/constants.py
@@ -2,24 +2,53 @@ import random
 import uuid
 
 DELIMITER = "\x1e"
+
+
 # Generate random IP between range 13.104.0.0/14
-FORWARDED_IP = f"11.{random.randint(104, 107)}.{random.randint(0, 255)}.{random.randint(0, 255)}"
+FORWARDED_IP = f"13.{random.randint(104, 107)}.{random.randint(0, 255)}.{random.randint(0, 255)}"
 
 HEADERS = {
     "accept": "application/json",
     "accept-language": "en-US;q=0.9",
     "accept-encoding": "gzip, deflate, br, zsdch",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.188",
+    "content-type": "text/stream; charset=UTF-8",
+    "sec-ch-ua": '"Not/A)Brand";v="99", "Microsoft Edge";v="115", "Chromium";v="115"',
+    "sec-ch-ua-arch": '"x86"',
+    "sec-ch-ua-bitness": '"64"',
+    "sec-ch-ua-full-version": '"115.0.1901.188"',
+    "sec-ch-ua-full-version-list": '"Not/A)Brand";v="99.0.0.0", "Microsoft Edge";v="115.0.1901.188", "Chromium";v="115.0.5790.114"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-model": "",
+    "sec-ch-ua-platform": '"Windows"',
+    "sec-ch-ua-platform-version": '"15.0.0"',
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "sec-ms-gec-version": "1-115.0.1901.188",
+    "x-ms-client-request-id": str(uuid.uuid4()),
     "x-ms-useragent": "azsdk-js-api-client-factory/1.0.0-beta.1 core-rest-pipeline/1.10.3 OS/Windows",
+    "Referer": "https://www.bing.com/search?",
+    "Referrer-Policy": "origin-when-cross-origin",
     "x-forwarded-for": FORWARDED_IP,
 }
 
 HEADERS_INIT_CONVER = {
-    "accept": "application/json",
+    "authority": "www.bing.com",
+    "accept": "text/stream; charset=UTF-8",
     "accept-language": "en-US;q=0.9",
-    "accept-encoding": "gzip, deflate, br, zsdch",
+    "cache-control": "max-age=0",
+    "sec-ch-ua": '"Not/A)Brand";v="99", "Microsoft Edge";v="115", "Chromium";v="115"',
+    "sec-ch-ua-arch": '"x86"',
+    "sec-ch-ua-bitness": '"64"',
+    "sec-ch-ua-full-version": '"115.0.1901.188"',
+    "sec-ch-ua-full-version-list": '"Not/A)Brand";v="99.0.0.0", "Microsoft Edge";v="115.0.1901.188", "Chromium";v="115.0.5790.114"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-model": '""',
+    "sec-ch-ua-platform": '"Windows"',
+    "sec-ch-ua-platform-version": '"15.0.0"',
+    "upgrade-insecure-requests": "1",
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.188",
-    "x-ms-useragent": "azsdk-js-api-client-factory/1.0.0-beta.1 core-rest-pipeline/1.10.3 OS/Windows",
+    "x-edge-shopping-flag": "1",
     "x-forwarded-for": FORWARDED_IP,
 }
 
