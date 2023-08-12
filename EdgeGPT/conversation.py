006dfc47d2c58102b5233356f4beb7e5086530d7
renqabs <130155002+renqabs@users.noreply.github.com>
2023-08-12 11:52:33 +0800

    Update conversation.py

--- EdgeGPT/conversation.py
+++ EdgeGPT/conversation.py
@@ -107,7 +107,7 @@ class Conversation:
             timeout=30,
             headers={
                 **HEADERS_INIT_CONVER,
-                "x-forwarded-for": f"11.{random.randint(104, 107)}.{random.randint(0, 255)}.{random.randint(0, 255)}"
+                "x-forwarded-for": f"13.{random.randint(104, 107)}.{random.randint(0, 255)}.{random.randint(0, 255)}"
                     },
             transport=transport,
             cookies=formatted_cookies,
