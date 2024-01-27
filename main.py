94c621177a13f8f01d948941dad847bcc63f8a93
renqabs <130155002+renqabs@users.noreply.github.com>
2024-01-27 17:34:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ async def sydney_process_message(user_message, bot_mode, context, _U, KievRPSSec
     chatbot = None
     cookies = loaded_cookies
     image_gen_cookie = []
+    '''
     if _U:
         image_gen_cookie += [{"name": "_U", "value": _U}]
     if KievRPSSecAuth:
@@ -37,6 +38,7 @@ async def sydney_process_message(user_message, bot_mode, context, _U, KievRPSSec
                  }
     cookies += [{"name": "SRCHHPGUSR", "value": SRCHHPGUSR[bot_mode]}]
     image_gen_cookie += [{"name": "SRCHHPGUSR", "value": "SRCHLANG=zh-Hans&" + SRCHHPGUSR[bot_mode]}]
+    '''
     os.environ['image_gen_cookie'] = json.dumps(image_gen_cookie)
     # Set the maximum number of retries
     max_retries = 5
