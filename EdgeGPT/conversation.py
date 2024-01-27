b9a5f874fb62154b06bc4c85d456bc5d60e3c92b
renqabs <130155002+renqabs@users.noreply.github.com>
2024-01-27 17:59:48 +0800

    Update conversation.py

--- EdgeGPT/conversation.py
+++ EdgeGPT/conversation.py
@@ -102,7 +102,7 @@ class Conversation:
             formatted_cookies = httpx.Cookies()
             for cookie in cookies:
                 formatted_cookies.set(cookie["name"], cookie["value"])
-        os.environ['xff'] = "23.142.65.13" #get_random_ip()
+        os.environ['xff'] = get_random_ip()
         async with httpx.AsyncClient(
             proxies=proxy,
             timeout=30,
